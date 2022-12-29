cb315660d03fb636b7f9f7c01c16e25804b03782
otoo0320 <iris4728124@gmail.com>
2022-12-29 21:16:41 +0900

    모르겠음
    연구해보자..

--- HR-day07-PL.sql
+++ HR-day07-PL.sql
@@ -19,7 +19,7 @@ SET SERVEROUTPUT ON
 -- 스크립트 경과 시간을 출력하도록 설정
 SET TIMING ON
 
-DECLARE -- 변수를 정의 하는 영역
+DECLARE --변수를 정의 하는 영역
     V_STRD_DT VARCHAR(8)
     V_STRD_DEPTNO NUMBER;
     
@@ -39,7 +39,7 @@ BEGIN -- 작업 영역
         SELECT T1. department_id
             , T1.department_name
             , T1.location_id
-        INTO V_DEPTNO
+        INTO V_DEPTNO  --select의 결과를 into의 변수들에 담아줌
             ,V_DNAME
             ,V_LOC
         FROM departments T1
@@ -63,20 +63,179 @@ END; --작업종료
     
 --오류남ㅡㅡ 왜그런지 쌤 거랑 비교하면서 찾기
     
+desc departments;    
     
+/*
+프로시저
+
+[기본구조]
+CREATE OR REPLACE PROCEDURE 프로시져이름 (파라미터1, 파라미터2...);
+    IS[AS]
+        선언부
+    BEGIN 
+        [실행부 - PL/SQL BLOCK]
+    [EXVEPTION]
+        [EXVEPTION] 처리
+END;
+*/
+
+CREATE OR REPLACE PROCEDURE print_hello_proc
+    IS
+        msg VARCHAR2(20) :='hello world';  --변수 초기값 선언
+    BEGIN --문장의 시작
+        DBMS_OUTPUT.PUT_LINE(ㅡㄶ);
+    END; -- 문장의 끝
+--프로시저 종료
     
+EXEC print_hello_proc;
+
+CREATE TABLE emp2 AS
+ SELECT * FROM employees;
+
+-- IN 매개변수
+CREATE OR REPLACE PROCEDURE update_emp2_salary_proc(eno IN NUMBER) IS
+    BEGIN
+        UPDATE emp2 SET salary = salary*1.1
+        WHERE employee_id = eno;
+        COMMIT;
+    END;
     
+ select * from emp2
+ WHERE employee_id =115;
     
+ EXEC update_emp2_salary_proc(115);
+ 
+ 
+-- OUT 매개변수 / call by reference
+-- OUT : 프로시저는 반환값이 없으므로 OUT 매개변수 활용
+CREATE OR REPLACE PROCEDURE find_emp2_proc(v_eno IN NUMBER,
+    v_fname OUT NVARCHAR2, v_lname OUT NVARCHAR2, v_sal OUT NUMBER)
+    IS
+    BEGIN
+        SELECT first_name, last_name, salary
+        INTO v_fname, v_lname, v_sal
+        FROM emp2 WHERE employee_id =v_eno;
+    END;
     
+VARIABLE v_fname NVARCHAR2(25);
+VARIABLE v_lname NVARCHAR2(25);
+VARIABLE v_sal NUMBER(8,2);
+
+EXECUTE find_emp2_proc(115, :v_fname, :v_lname, :v_sal);
+PRINT v_fname;
+PRINT v_lname;
+PRINT v_sal;
     
+-- IN OUT  매개변수
+-- 매개변수로 시작하고 반환변수로 끝난다.
+CREATE OR REPLACE PROCEDURE find_emp2_sal(v_eno IN OUT NUMBER)
+IS 
+    BEGIN
+       SELECT salary
+       INTO v_eno
+       FROM emp2 WHERE employee_id = v_eno;
+    END;
     
+ DECLARE
+    v_eno NUMBER :=115;
+    BEGIN 
+        DBMS_OUTPUT.PUT_LINE('eno ='||v_eno);
+        find_emp2_sal(v_eno);
+        DBMS_OUTPUT.PUT_LINE('eno ='||v_eno)
+    END;
     
+VAR v_eno NUMBER;
+EXEC :v_eno := 115;
+PRINT v_eno;
+EXEC find_emp2_sal(:v_eno);
+PRINT v_eno;
     
+/*
+함수 (Function)
+    특정 기능들을 모듈화, 재사용 할 수 있어서 복잡한 쿼리문을 간결하게 만들 수 있음
     
+[기본구조]
+CREATE OR REPLACE FUNCTION 함수명 (파라미터1, 파라미터2...);
+RETURN datatype -- 반환되는 값의 datatype
+    IS [AS]
+        선언부
+    BEGIN 
+        [실행부 - PL/SQL BLOCK]
+    [EXCEPTION]
+        [EXCEPTION] 처리
+    RETURN 변수;
+END;
+*/
+
+CREATE OR REPLACE FUNCTION FN_GET_DEPT_NAME(
+    P_DEPT_NO IN_NUMBER
+) RETURN VARCHAR2
+    IS
+        V_TEST_NAME VARCHAR(30);
+    BEGIN
+        SELECT department_name
+        INTO   V_TEST_NAME
+        FROM   departments
+        WHERE  department_id = P_DEPT_NO;
+        
+    RETURN      V_TEST_NAME;
+    END;
     
+SELECT FN_GET_DEPT_NAME(20) FROM dual;
     
+/*
+트리거(TRIGGER)
+    INSERT, UPDATE, DELETE(DML) 문이 TABLE에 대해 행해질 때 묵시적으로 수행되는 프로시저
     
+[기본구조]
+CREATE OR REPLACE TRIGGER 트리거명
+    - 트리거 옵션
+    BEFORE OR AFTER
+    INSERT OR UPDATE OR DELETE ON 테이블명
+    [FOR EACH ROW]
+DECLARE
+    선언부;
+BEGIN
+    실행부
+    [INSERTING, UPDATING, DELETING]
+[EXCEPTION]
+    예외처리부;
+END;
+*/
+
+CREATE TABLE dept6(
+    deptno NUMBER(6) PRIMARY KEY,
+    dname VARCHAR(200),
+    loc VARCHAR2(200),
+    create_date DATE DEFAULT SYSDATE,
+    update_data DATE DEFAULT SYSDATE
+    );
     
+CREATE OR REPLACE TRIGGER tr_dept6
+    BEFORE UPDATE ON dept6
+    FOR EACH ROW
+    BEGIN
+        :new.update_data := SYSDATE;
+    END;
+    
+
+INSERT INTO dept6(deptno, dname, loc)
+VALUES (1, 'DEV', '서울특별시');
+COMMIT;
+    
+SELECT 
+deptno, 
+dname, 
+loc, 
+TO_CHAR(create_date, 'YYYYMMDD HH24:MI:SS'), 
+TO_CHAR(update_date, 'YYYYMMDD HH24:MI:SS')
+FROM dept6;    
+    
+UPDATE dept6 SET
+loc = '강릉시'
+WHERE deptno = 1;
+    
+--뭔가 안됨..    
     
     
     
